b8b025fd93cfa265a4811c99125f7f445aa066fe
Shamim Hossain <shossain@secureship.ca>
2023-10-16 14:13:18 +0600

    feat: add basic hero section

---
+++ app/(marketing)/_components/heros.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+
+const Heros = () => {
+  return (
+    <div className='flex max-w-5xl flex-col items-center justify-center'>
+      <div className='flex items-center'>
+        <div className='relative h-[300px] w-[300px] sm:h-[350px] sm:w-[350px] md:h-[400px] md:w-[400px]'>
+          <Image
+            src='/documents.png'
+            fill
+            className='object-contain'
+            alt='Documents'
+          />
+        </div>
+
+        <div className='relative hidden h-[400px] w-[400px] md:block'>
+          <Image
+            src='/reading.png'
+            fill
+            className='object-contain'
+            alt='Documents'
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Heros;
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,12 @@
 import Heading from './_components/heading';
+import Heros from './_components/heros';
 
 export default function Home() {
   return (
     <div className='flex min-h-full flex-col'>
-      <div className='flex flex-1 flex-col items-center justify-center px-6 pb-10 text-center'>
+      <div className='flex flex-1 flex-col items-center justify-center gap-y-8 px-6 pb-10 text-center'>
         <Heading />
+        <Heros />
       </div>
       <div className='bg-green-300'>Footer</div>
     </div>
